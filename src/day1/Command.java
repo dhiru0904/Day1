55e06a3329810c098611f768b8e6bddf55049a35
dhiru0904 <dhirajdevre4@gmail.com>
2022-10-06 15:31:53 +0530

    Added:day 1 Assignment

---
+++ src/day1/Command.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Command {
+
+    public static void main(String[] args) {
+            System.out.println("Your first argument is: "+args[0]);
+
+        }
+    }
---
+++ src/day1/String.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class String {
+    public static void main(java.lang.String[] args)
+    {
+        java.lang.String s1="dhiru";
+        java.lang.String s2="dhiru";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
+
+
